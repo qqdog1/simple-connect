edb2c830a5bd0aa46d1dae83c41a926b6b2d1e75
shawn <Shawn.Chou@nogle.com>
2015-08-07 16:07:15 +0800

    add unpacking case

--- common/src/test/java/name/qd/simpleConnect/common/packer/SimpleConnectDataPackerBenchmark.java
+++ common/src/test/java/name/qd/simpleConnect/common/packer/SimpleConnectDataPackerBenchmark.java
@@ -1,15 +1,26 @@
 package name.qd.simpleConnect.common.packer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 import name.qd.simpleConnect.common.enumeration.OP_CodeEnum;
 import name.qd.simpleConnect.common.packer.vo.PackVo;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -18,46 +29,91 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @State(Scope.Benchmark)
 public class SimpleConnectDataPackerBenchmark {
 
+	private static final int WARMUP_COUNT = 5;
+	private static final int MEANSURE_COUNT = 5;
+
 	public static void main(String[] s) throws RunnerException {
 		Options opt = new OptionsBuilder()
 				.include(SimpleConnectDataPackerBenchmark.class.getSimpleName())
-				.warmupIterations(5)
-                .measurementIterations(5)
-//                .threads(4)
+				.warmupIterations(WARMUP_COUNT)
+				.measurementIterations(MEANSURE_COUNT)
+//				.threads(4)
 				.timeUnit(TimeUnit.MICROSECONDS)
+//				.mode(Mode.AverageTime)
+//				.mode(Mode.SingleShotTime)
 				.forks(1)
 				.build();
 		new Runner(opt).run();
 	}
-	
+
 	private PackVo vo;
-	
+	private InputStream inputStream;
+
 	@Setup(Level.Trial)
-    public void setUp() {
-        vo = new PackVo();
+	public void init() {
+		// init Vo
+		vo = new PackVo();
 		vo.setData("QQKK123".getBytes());
 		vo.setOP_CodeEnum(OP_CodeEnum.DATA);
-    }
-	
+
+		// init InputStream
+		try {
+			File file = new File("PackVoByte");
+			FileOutputStream fOut = new FileOutputStream(file);
+			file.createNewFile();
+			for (int i = 0; i < MEANSURE_COUNT + WARMUP_COUNT; i++) {
+				fOut.write(SimpleConnectDataPacker.packingDataUseByteBuffer(vo));
+				fOut.flush();
+			}
+			fOut.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			File file = new File("PackVoByte");
+			inputStream = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@TearDown
+	public void release() {
+		try {
+			inputStream.close();
+			Files.deleteIfExists(Paths.get("PackVoByte"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Benchmark
 	public void newVo() {
 		PackVo testVo = new PackVo();
 		testVo.setData("QQKK123".getBytes());
 		testVo.setOP_CodeEnum(OP_CodeEnum.DATA);
 	}
-	
+
 	@Benchmark
 	public void packing() {
 		SimpleConnectDataPacker.packingData(vo);
 	}
-	
+
 	@Benchmark
 	public void packingUseByteBuffer() {
 		SimpleConnectDataPacker.packingDataUseByteBuffer(vo);
 	}
-	
+
 	@Benchmark
+	@BenchmarkMode(Mode.SingleShotTime)
 	public void unpack() {
-		
+		try {
+			SimpleConnectDataPacker.unpackingData(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- common/src/test/java/name/qd/simpleConnect/common/packer/SimpleConnectDataPackerTest.java
+++ common/src/test/java/name/qd/simpleConnect/common/packer/SimpleConnectDataPackerTest.java
@@ -2,6 +2,12 @@ package name.qd.simpleConnect.common.packer;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
 import name.qd.simpleConnect.common.enumeration.OP_CodeEnum;
@@ -22,6 +28,38 @@ public class SimpleConnectDataPackerTest {
 	
 	@Test
 	public void UnpackingTest() {
+		// init Vo
+        PackVo vo = new PackVo();
+		vo.setData("QQKK123".getBytes());
+		vo.setOP_CodeEnum(OP_CodeEnum.DATA);
 		
+		// init InputStream
+		try {
+			File file = new File("PackVoByte");
+			FileOutputStream fOut = new FileOutputStream(file);
+			file.createNewFile();
+			fOut.write(SimpleConnectDataPacker.packingDataUseByteBuffer(vo));
+			fOut.flush();
+			fOut.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		InputStream inputStream = null;
+		try {
+			File file = new File("PackVoByte");
+			inputStream = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			PackVo vo2 = SimpleConnectDataPacker.unpackingData(inputStream);
+			System.out.println(new String(vo.getData()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
