48af7656d68de30b50f470987921a678e467477e
shawn <Shawn.Chou@nogle.com>
2018-02-21 18:18:46 +0800

    1. cleanup

--- common/src/main/java/name/qd/simpleConnect/common/constant/SimpleConnectVersion.java
+++
@@ -1,6 +0,0 @@
-package name.qd.simpleConnect.common.constant;
-
-public class SimpleConnectVersion {
-	public static final String VERSION = "0.1.1";
-	public static final String DATE = "20180221";
-}
